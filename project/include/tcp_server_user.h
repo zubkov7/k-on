545b7bee76fffb1e8b2e504fd3188d3ade662def
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-11-23 00:20:01 +0300

    add tcp_server_user interface

---
+++ project/include/tcp_server_user.h
@@ -0,0 +1,25 @@
+//
+// Created by andrey on 23.11.2019.
+//
+
+#ifndef K_ON_TCP_SERVER_USER_H
+#define K_ON_TCP_SERVER_USER_H
+
+#include "user.h"
+
+class TcpServerUser(TcpServer) {
+public:
+    TcpServerUser();
+    ~TcpServerUser();user on_get_user(const int id);
+
+private:
+    User user_;
+    void handle_request(const std::string request);
+    bool on_login(const std::string &login, const std::string &pass);
+    bool on_register(const std::string &login, const std::string &pass);
+    int on_inc_listening(const int song_id, const int user_id);
+    bool on_like_song(const int song_id, const int user_id, const bool value);
+};
+
+
+#endif //K_ON_TCP_SERVER_USER_H
