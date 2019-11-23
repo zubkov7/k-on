7e1ee3487e99c1f7fe8bed82f0cce030db9dbe17
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-11-23 13:00:06 +0300

    db_worker_user: remove struct user (now it is located in db_entities), update parameters of constructor and add new methods

--- project/include/db_worker_user.h
+++ project/include/db_worker_user.h
@@ -4,26 +4,25 @@
 #include <string>
 #include <map>
 
-struct user {
-    int id;
-    std::string login;
-    std::string pass;
-
-    user(const int id, const std::string &login, const std::string &pass) : id(id), login(login), pass(pass) {}
-};
+#include "db_entities.h"
 
 class DbWorkerUser {
 public:
-    DbWorkerUser(const std::string &user, const std::string &pass);
+    DbWorkerUser(const std::string &db_path, const std::string &host,
+                 const std::string &login, const std::string &pass);
     ~DbWorkerUser() {}
     user get_user(const int id);
     bool login(const std::string &login, const std::string &pass);
     bool register_user(const std::string &login, const std::string &pass);
     int increment_listening(const int song_id, const int user_id);
     bool like_song(const int song_id, const int user_id, const bool value);
+    int get_listening(const int song_id, const int user_id);
+    bool get_like_song(const int song_id, const int user_id);
 
 private:
-    DbWrapper db_wrapper;
+    DbWrapper _db_wrapper;
+
+private:
     bool is_login_available(const std::string &login);
 };
 
