d99e8c5462e04fd30ae7922ab6084952b1e66b8a
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-12-07 00:46:58 +0300

    DBWorkerUser class implementation with plugs

---
+++ project/include/DbWorkerUser.h
@@ -0,0 +1,30 @@
+#ifndef K_ON_DB_WORKER_USER_H
+#define K_ON_DB_WORKER_USER_H
+
+#include <string>
+#include <map>
+
+#include "db_entities.h"
+#include "DbWrapper.h"
+
+class DbWorkerUser {
+public:
+    DbWorkerUser();
+    ~DbWorkerUser();
+    user get_user(int id);
+    bool login(const std::string &login, const std::string &pass);
+    bool register_user(const std::string &login, const std::string &pass);
+    int increment_listening(int song_id, int user_id);
+    bool like_song(int song_id, int user_id, bool value);
+    int get_listening(int song_id, int user_id);
+    bool get_like_song(int song_id, int user_id);
+
+private:
+    DbWrapper db_wrapper_;
+
+private:
+    bool is_login_available(const std::string &login);
+};
+
+
+#endif  // K_ON_DB_WORKER_USER_H
---
+++ project/src/DbWorkerUser.cpp
@@ -0,0 +1,56 @@
+#include "DbWorkerUser.h"
+
+DbWorkerUser::DbWorkerUser() {
+    // TODO() : определить конструктор
+}
+
+DbWorkerUser::~DbWorkerUser() {
+    // TODO() : определить деструктор
+}
+
+user DbWorkerUser::get_user(int id) {
+    // TODO() : выполнить нужный вопрос через DbWrapper
+    db_wrapper_.execute_query("select * from user;");
+    return user(0, "login", "password");
+}
+
+bool DbWorkerUser::login(const std::string &login, const std::string &pass) {
+    // TODO() : выполнить нужный вопрос через DbWrapper
+    db_wrapper_.execute_query("select * from user;");
+    return false;
+}
+
+bool DbWorkerUser::register_user(const std::string &login, const std::string &pass) {
+    // TODO() : выполнить нужный вопрос через DbWrapper
+    db_wrapper_.execute_query("select * from user;");
+    return false;
+}
+
+int DbWorkerUser::increment_listening(int song_id, int user_id) {
+    // TODO() : выполнить нужный вопрос через DbWrapper
+    db_wrapper_.execute_query("select * from user;");
+    return 0;
+}
+
+bool DbWorkerUser::like_song(int song_id, int user_id, bool value) {
+    // TODO() : выполнить нужный вопрос через DbWrapper
+    db_wrapper_.execute_query("select * from user;");
+    return false;
+}
+
+int DbWorkerUser::get_listening(int song_id, int user_id) {
+    // TODO() : выполнить нужный вопрос через DbWrapper
+    db_wrapper_.execute_query("select * from user;");
+    return 0;
+}
+
+bool DbWorkerUser::get_like_song(int song_id, int user_id) {
+    // TODO() : выполнить нужный вопрос через DbWrapper
+    db_wrapper_.execute_query("select * from user;");
+    return false;
+}
+
+bool DbWorkerUser::is_login_available(const std::string &login) {
+    // TODO() : выполнить нужный вопрос через DbWrapper
+    return false;
+}
