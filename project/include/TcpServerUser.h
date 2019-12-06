d658ace0e012abef95fb2260b1ff4a3304729303
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-12-07 00:45:37 +0300

    TcpServerUser class implementation

---
+++ project/include/TcpServerUser.h
@@ -0,0 +1,29 @@
+//
+// Created by andrey on 23.11.2019.
+//
+
+#ifndef K_ON_TCP_SERVER_USER_H
+#define K_ON_TCP_SERVER_USER_H
+
+#include <string>
+
+#include "User.h"
+#include "TcpServer.h"
+
+class TcpServerUser : public TcpServer {
+public:
+    TcpServerUser(unsigned short port);
+private:
+    User user_;
+    
+private:
+    std::string handle_request(const std::string &request) override;
+    std::string on_get_user(int id);
+    std::string on_login(const std::string &login, const std::string &pass);
+    std::string on_register(const std::string &login, const std::string &pass);
+    std::string on_inc_listening(int song_id, int user_id);
+    std::string on_like_song(int song_id, int user_id, bool value);
+};
+
+
+#endif  // K_ON_TCP_SERVER_USER_H
---
+++ project/src/TcpServerUser.cpp
@@ -0,0 +1,58 @@
+//
+// Created by andrey on 23.11.2019.
+//
+
+#include "TcpServerUser.h"
+
+TcpServerUser::TcpServerUser(unsigned short port) : TcpServer(port) {}
+
+std::string TcpServerUser::handle_request(const std::string &request) {
+    // TODO() : по запросу вызывает нужные методы (сервера) on_get_user...
+    if (request == "get user") {
+        return on_get_user(1);
+    }
+    if (request == "login") {
+        return on_login("login", "password");
+    }
+    if (request == "register") {
+        return on_register("login", "password");
+    }
+    if (request == "increment listening") {
+        return on_inc_listening(1, 1);
+    }
+    if (request == "like song") {
+        return on_like_song(1, 1, true);
+    }
+
+    return "Wrong command!";
+}
+
+std::string TcpServerUser::on_get_user(int id) {
+    // TODO() : вызывается метод класса User.get_user() и генерируется ответ в виде строки
+    user_.get_user(1);
+    return "Answer on get user";
+}
+
+std::string TcpServerUser::on_login(const std::string &login, const std::string &pass) {
+    // TODO() : вызывается метод класса User.login() и генерируется ответ в виде строки
+    user_.login("login", "password");
+    return "Answer on user login";
+}
+
+std::string TcpServerUser::on_register(const std::string &login, const std::string &pass) {
+    // TODO() : вызывается метод класса User.register_user() и генерируется ответ в виде строки
+    user_.register_user("login", "password");
+    return "Answer on user register";
+}
+
+std::string TcpServerUser::on_inc_listening(int song_id, int user_id) {
+    // TODO() : вызывается метод класса User.increment_listening() и генерируется ответ в виде строки
+    user_.increment_listening(1, 1);
+    return "Answer on increment user listening";
+}
+
+std::string TcpServerUser::on_like_song(int song_id, int user_id, bool value) {
+    // TODO() : вызывается метод класса User.like_song() и генерируется ответ в виде строки
+    user_.like_song(1, 1, true);
+    return "Answer on user like song";
+}
