2ce56445a2c2001058a141aaaefc69cfd174f68b
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-12-07 01:07:06 +0300

    add main for user server

---
+++ project/src/MainUserServer.cpp
@@ -0,0 +1,22 @@
+#include "TcpServerUser.h"
+
+#define PORT 7777
+
+int main(int argc, char *argv[]) {
+    try {
+        TcpServerUser server(PORT);
+        server.start_server();
+    }
+    catch (boost::system::system_error const &e) {
+        if (e.code() == boost::asio::error::address_in_use) {
+            std::cout << "Address already in use\n";
+        } else {
+            boost::throw_exception(e);
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception in thread: " << e.what() << "\n";
+    }
+
+    return 0;
+}
