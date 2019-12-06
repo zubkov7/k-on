b99a6431c852b5676485132bb84d32a8e75de28d
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-12-06 23:27:02 +0300

    add try catch in TcpClient class

--- project/src/TcpClient.cpp
+++ project/src/TcpClient.cpp
@@ -12,7 +12,9 @@ TcpClient::TcpClient() :
     resolver_(boost::asio::ip::tcp::resolver(this->service_)) {}
 
 TcpClient::~TcpClient() {
-    socket_.close();
+    if (socket_.is_open()) {
+        socket_.close();
+    }
 }
 
 std::string TcpClient::to_string(boost::asio::streambuf &buf) {
@@ -22,26 +24,67 @@ std::string TcpClient::to_string(boost::asio::streambuf &buf) {
 }
 
 void TcpClient::close_connection() {
-    socket_.close();
+    if (socket_.is_open()) {
+        socket_.close();
+    }
 }
 
 void TcpClient::connect(char *host, char *port) {
     this->host_ = std::string(host);
     this->port_ = boost::lexical_cast<unsigned short>(port);
 
-    boost::asio::connect(this->socket_, this->resolver_.resolve({host, port}));
+    try {
+        boost::asio::connect(this->socket_, this->resolver_.resolve({host, port}));
+    }
+    catch (boost::system::system_error const &e) {
+        if (e.code() == boost::asio::error::connection_refused) {
+            std::cout << "Unable to connect to server " << host << ":" << port << std::endl;
+        } else {
+            boost::throw_exception(e);
+        }
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 }
 
 void TcpClient::write(const std::string &message) {
     boost::asio::streambuf answer;
     std::ostream out(&answer);
     out << message << std::endl;
-    boost::asio::write(this->socket_, answer);
+    try {
+        boost::asio::write(this->socket_, answer);
+    }
+    catch (boost::system::system_error const &e) {
+        if (e.code() == boost::asio::error::bad_descriptor || e.code() == boost::asio::error::broken_pipe) {
+            std::cout << "The connection to the server is closed\n";
+        } else {
+            boost::throw_exception(e);
+        }
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 }
 
 std::string TcpClient::read() {
     boost::asio::streambuf received_data;
-    boost::asio::read_until(this->socket_, received_data, READ_UNTIL_DELIM);
+
+    try {
+        boost::asio::read_until(this->socket_, received_data, READ_UNTIL_DELIM);
+    }
+    catch (boost::system::system_error const &e) {
+        if (e.code() == boost::asio::error::bad_descriptor || e.code() == boost::asio::error::not_connected) {
+            std::cout << "The connection to the server is closed\n";
+        } else if (e.code() == boost::asio::error::eof) {
+            std::cout << "Server closed connection\n";
+        } else {
+            boost::throw_exception(e);
+        }
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 
     std::string received_data_str = this->to_string(received_data);
     return received_data_str;
