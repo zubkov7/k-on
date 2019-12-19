0c37b8c0e0bba4712cf76540e6a5bda76b0118cc
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-12-20 00:37:22 +0300

    fix commit

--- project/include/db_worker_user.h
+++ project/include/db_worker_user.h
@@ -10,12 +10,15 @@ class DbWorkerUser {
 public:
     DbWorkerUser();
     ~DbWorkerUser();
-    bool login(const std::string &login, const std::string &pass) const;
-    bool signup(const std::string &login, const std::string &pass) const;
+    std::string login(const std::string &login, const std::string &pass, bool &status) const;
+    std::string signup(const std::string &login, const std::string &pass, bool &status) const;
     int increment_listening(int song_id, int user_id) const;
     bool like_song(int song_id, int user_id, bool value) const;
     bool is_user_exists(int user_id) const;
     bool is_song_exists(int song_id) const;
+    int get_user_id(const std::string &session, bool &status) const;
+    void logout(const std::string &session) const;
+    std::string get_login(const std::string session, bool &status) const;
 
 private:
     DbWrapper db_wrapper_;
