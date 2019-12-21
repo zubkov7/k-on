4a8b910e1688dff21060409b687c2253745acf8a
AliceSitedge <a.seledkina@mail.ru>
2019-12-21 04:00:59 +0300

    server implemented

--- project/src/tcp_server_recommendations.cpp
+++ project/src/tcp_server_recommendations.cpp
@@ -1,5 +1,3 @@
-
-
 #include "tcp_server_recommendations.h"
 #include "json.h"
 
@@ -44,12 +42,12 @@ std::string TcpServerRecommendations::handle_request(const std::string &request)
     }
 }
 
-std::string songs_to_answer(const std::vector<Song> &songs) const {
+std::string TcpServerRecommendations::songs_to_answer(const std::vector<Song> &songs) const {
     boost::property_tree::ptree answer;
     answer.put("status", 200);
 
     boost::property_tree::ptree songs_array;
-    for (const auto song : songs) {
+    for (const auto& song : songs) {
         boost::property_tree::ptree element;
         element.put("id", song.id);
         element.put("name", song.name);
@@ -58,9 +56,10 @@ std::string songs_to_answer(const std::vector<Song> &songs) const {
         element.put("duration", song.duration);
         element.put("date", song.date);
 
-        songs_array.push_back(element);
+        songs_array.push_back(std::make_pair("", element));
     }
     answer.put("songs", songs_array);
+
     return stringify_json(answer);
 }
 
@@ -78,18 +77,56 @@ std::string TcpServerRecommendations::on_recommendations(const boost::property_t
     return songs_to_answer(songs);
 }
 
-std::string on_top(const boost::property_tree::ptree &root) const {
+std::string TcpServerRecommendations::on_top(const boost::property_tree::ptree &root) const {
+    std::vector<Song> songs;
+    try {
+        songs = recommendation_system.get_popular(NUMBER_OF_SONGS);
+    } catch (sql::SQLException &e) {
+        std::cout << e.what() << std::endl;
+        return on_fail(500, "Database error");
+    }
 
+    return songs_to_answer(songs);
 }
 
-std::string on_recent(const boost::property_tree::ptree &root) const {
+std::string TcpServerRecommendations::on_recent(const boost::property_tree::ptree &root) const {
+    std::vector<Song> songs;
+    try {
+        songs = recommendation_system.get_new(NUMBER_OF_SONGS);
+    } catch (sql::SQLException &e) {
+        std::cout << e.what() << std::endl;
+        return on_fail(500, "Database error");
+    }
 
+    return songs_to_answer(songs);
 }
 
-std::string on_similar(const boost::property_tree::ptree &root) const {
+std::string TcpServerRecommendations::on_similar(const boost::property_tree::ptree &root) {
+    int song = root.get<int>("song_id");
 
+    std::vector<Song> songs;
+    try {
+        songs = recommendation_system.get_similar(song, NUMBER_OF_SONGS);
+    } catch (sql::SQLException &e) {
+        std::cout << e.what() << std::endl;
+        return on_fail(500, "Database error");
+    }
+
+    return songs_to_answer(songs);
 }
 
-std::string on_update(const boost::property_tree::ptree &root) const {
+std::string TcpServerRecommendations::on_update(const boost::property_tree::ptree &root) {
+    int user = root.get<int>("user_id");
 
+    std::vector<Song> songs;
+    try {
+        recommendation_system.update_recommendations(user);
+    } catch (sql::SQLException &e) {
+        std::cout << e.what() << std::endl;
+        return on_fail(500, "Database error");
+    }
+
+    boost::property_tree::ptree answer;
+    answer.put("status", 200);
+    return stringify_json(answer);
 }
