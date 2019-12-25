db9ed22da02edbb394f392f19384630a4119161f
Oleg <elizarov1999@list.ru>
2019-12-25 20:00:22 +0300

    add tests

--- project/include/web_server.h
+++ project/include/web_server.h
@@ -15,7 +15,8 @@ public:
     void restart();
     void stop();
 
-private:
+//private:
+public:
     int port;
     boost::asio::ip::address host;
     int threads_num;
@@ -24,7 +25,8 @@ private:
     boost::asio::io_service service_;
     boost::asio::ip::tcp::acceptor acceptor_;
 
-private:
+//private:
+public:
     std::map<std::string, std::string> read_config();
     void on_accept(std::shared_ptr<Client> c, const boost::system::error_code &e);
     void start_accept();
--- project/test_server.cpp
+++ project/test_server.cpp
@@ -3,161 +3,58 @@
 #include <iostream>
 #include <fstream>
 #include "include/web_server.h"
+#include "include/client.h"
 #include <string>
+#include <boost/property_tree/json_parser.hpp>
+#include <regex>
+#include <map>
 
-/*
-TEST(NULL, check_timeout) {
-    std::string host = "127.0.0.1";
-    std::string result = "";
-    int port = 8000;
-    //default timeout will be 1 sec
-    float timeout = 0.00001;
-    char buffer[128];
 
-    std::ofstream outfile1("config.txt");
-    outfile1 << "workers:4 host:" + host + "port:" + (std::to_string(port)) + "timeout:" + (std::to_string(timeout))
-             << std::endl;
-    outfile1.close();
-
-    std::ofstream outfile2("index.html");
-    outfile2 << "<html><head></head><body>Hello , world!</body></html>" << std::endl;
-    outfile2.close();
-
-    Web_server *serv = new Web_server();
-    serv->start();
-
-    FILE *cmd = popen("telnet 127.0.0.1:8000", "r");
-    while (!feof(cmd)) {
-        // use buffer to read and add to result
-        if (fgets(buffer, 128, cmd) != NULL)
-            result += buffer;
-    }
-    pclose(cmd);
-    EXPECT_EQ("504 Gateway Timeout", result);
-}
-*/
-
-TEST(NULL, check_url) {
-    std::string host = "0.0.0.0";
-    std::string result = "";
-    int port = 5555;
-    char buffer[128];
-
-    std::ofstream outfile1("config.txt");
-    outfile1 << "NUM_THREADS:" + 4 + "HOST:" + host + "PORT:" + (std::to_string(port)) << std::endl;
-    outfile1.close();
-
-    std::ofstream outfile2("index.html");
-    outfile2 << "<html><head></head><body>Hello , world!</body></html>" << std::endl;
-    outfile2.close();
-
-    Web_server *serv = new Web_server();
-    serv->start();
-
-    FILE *cmd = popen("telnet 127.0.0.1:8000/badway", "r");
-    while (!feof(cmd)) {
-        // use buffer to read and add to result
-        if (fgets(buffer, 128, cmd) != NULL)
-            result += buffer;
-    }
-    pclose(cmd);
-    EXPECT_EQ("404 Not Found", result);
+TEST(NULL, test_parse_html) {
+    boost::asio::io_service service_;
+    std::shared_ptr<Client> c(new Client(service_));
+    std::string html =
+            c->parse_html("/Users/elenaelizarova/CLionProjects/k-on/project/index.html", "test1", "test2");
+    EXPECT_LT(html.find("test1"), html.size());
+    EXPECT_LT(html.find("test2"), html.size());
+    EXPECT_LT(html.find("test1"), html.find("test2"));
 }
 
-TEST(NULL, check_request) {
-    std::string host = "127.0.0.1";
-    std::string result = "";
-    int port = 8000;
-    char buffer[128];
 
-    std::ofstream outfile1("config.txt");
-    outfile1 << "workers:4 host:" + host + "port:" + (std::to_string(port)) << std::endl;
-    outfile1.close();
+TEST(NULL, test_json_to_songs) {
+    boost::asio::io_service service_;
+    std::shared_ptr<Client> c(new Client(service_));
 
-    std::ofstream outfile2("index.html");
-    outfile2 << "<html><head></head><body>Hello , world!</body></html>" << std::endl;
-    outfile2.close();
+    boost::property_tree::ptree response;
+    boost::property_tree::read_json("/Users/elenaelizarova/CLionProjects/k-on/project/package.json", response);
 
-    Web_server *serv = new Web_server();
-    serv->start();
-
-    FILE *cmd = popen("telnet 127.0.0.1:8000/?badparam=99", "r");
-    while (!feof(cmd)) {
-        // use buffer to read and add to result
-        if (fgets(buffer, 128, cmd) != NULL)
-            result += buffer;
-    }
-    pclose(cmd);
-    EXPECT_EQ("400 Bad Request", result);
+    std::string songs = c->json_to_songs(response);
+    EXPECT_LT(songs.find("/similarsong?song_id=1"), songs.size());
+    EXPECT_LT(songs.find("/similarsong?song_id=8"), songs.size());
+    EXPECT_LT(songs.find("/similarsong?song_id=4"), songs.find("/similarsong?song_id=8"));
 }
 
-TEST(NULL, check_start) {
-    std::string host = "127.0.0.1";
-    std::string result = "";
-    int port = 8000;
-    char buffer[128];
-
-    std::ofstream outfile1("config.txt");
-    outfile1 << "workers:4 host:" + host + "port:" + (std::to_string(port)) << std::endl;
-    outfile1.close();
-
-    std::ofstream outfile2("index.html");
-    outfile2 << "<html><head></head><body>Hello , world!</body></html>" << std::endl;
-    outfile2.close();
-
-    Web_server *serv = new Web_server();
-    serv->start();
-
-    FILE *cmd = popen("telnet 127.0.0.1:8000", "r");
-    while (!feof(cmd)) {
-        // use buffer to read and add to result
-        if (fgets(buffer, 128, cmd) != NULL)
-            result += buffer;
+TEST(NULL, test_read_config) {
+    Web_server serv("/Users/elenaelizarova/CLionProjects/k-on/project/config.txt");
+    std::stringstream buffer;
+    std::ifstream in("/Users/elenaelizarova/CLionProjects/k-on/project/config.txt"); //// окрываем файл для чтения
+    if (in.is_open()) {
+        buffer << in.rdbuf();
     }
-    pclose(cmd);
-    EXPECT_EQ("200 OK\n <html><head></head><body>Hello , world!</body></html>", result);
-}
+    in.close();
 
-TEST(NULL, check_stop) {
-    std::string host = "127.0.0.1";
-    std::string result = "";
-    int port = 8000;
-    char buffer[128];
-    std::ofstream outfile1("config.txt");
-    outfile1 << "workers:4 host:127.0.0.1 port:8000" << std::endl;
-    outfile1.close();
-    std::ofstream outfile2("index.html");
-    outfile2 << "<html><head></head><body>Hello , world!</body></html>" << std::endl;
-    outfile2.close();
-    Web_server *serv = new Web_server();
-    serv->start();
-    serv->stop();
-    FILE *cmd = popen("telnet 127.0.0.1:8000", "r");
-    while (!feof(cmd)) {
-        // use buffer to read and add to result
-        if (fgets(buffer, 128, cmd) != NULL)
-            result += buffer;
-    }
-    pclose(cmd);
-    EXPECT_EQ("Unable to connect to remote host", result);
-}
+    std::string port = buffer.str().substr(
+            buffer.str().find("PORT: ") + 6, buffer.str().find("HOST") - 7);
+    std::string host = buffer.str().substr(
+            buffer.str().find("HOST: ") + 6, buffer.str().find("N") - 18);
 
-TEST(NULL, check_restart) {
-    std::ofstream outfile1("config.txt");
-    outfile1 << "workers:4 host:127.0.0.1 port:8000" << std::endl;
-    Web_server *serv = new Web_server();
-    serv->start();
-    std::string line;
-    std::string line1;
-    std::ifstream myfile("pid.txt");
-    if (myfile.is_open()) {
-        while (getline(myfile, line)) {}
-        myfile.close();
-    }
-    serv->restart();
-    if (myfile.is_open()) {
-        while (getline(myfile, line1)) {}
-        myfile.close();
-    }
-    EXPECT_NE(line, line1);
+    std::string numbr = buffer.str().substr(
+            buffer.str().find("NUM_THREADS: ")+13);
+
+
+    std::map<std::string, std::string> m = serv.read_config();
+    EXPECT_EQ(m["PORT"], port);
+    EXPECT_EQ(m["NUM_THREADS"],numbr);
+    EXPECT_EQ(m["HOST"], host);
+
 }
